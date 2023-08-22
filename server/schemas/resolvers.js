f4416ad0ae98b3a3dda66127fe8c27add4053c15
Joseph Pham <joeypham13@gmail.com>
2023-08-21 20:04:49 -0400

    downloaded packages

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,40 @@
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+const resolvers = {
+  Query: {
+    me: async (parent, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw AuthenticationError;
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw AuthenticationError;
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      },
+      
+
+  }
+};
+
+module.exports = resolvers;
