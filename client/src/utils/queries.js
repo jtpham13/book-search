5aebda2927314601f06f94f6961c856878322efb
Joseph Pham <joeypham13@gmail.com>
2023-08-21 20:02:08 -0400

    added query me

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+query me {
+    me {
+      _id
+      bookCount
+      email
+      username
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+    }
+  }`
